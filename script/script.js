74c7411db576b5338a9e75c3472cca89391e6c9d
Andy212 <xfintx@tut.by>
2020-11-19 21:35:21 +0200

    Update script.js
    Исправил переменные

--- script/script.js
+++ script/script.js
@@ -9,11 +9,3 @@ let period;
 
 alert('переходи на темную сторону');
 console.log('У нас есть печеньки');
-console.log( money);
-console.log(income);
-console.log(addExpenses);
-console.log(deposit);
-console.log(mission);
-console.log(period);
-
-console.log(period);
