b26656878a5909aafd5caa5e69fd5322b8b7de4a
Andrei Dzemichau <Andyuson24@gmail.com>
2020-11-25 21:19:27 +0200

    исправил функции

--- script/script.js
+++ script/script.js
@@ -1,20 +1,15 @@
 'use strict';
 
-let money = 1000;
+let money = +prompt('Ваш месячный доход?');
 let income = 'разработка приложений';
-let addExpenses;
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 1000000;
 let period = 6;
 let budgetDay;
 let expenses = 0;
 let target = 0;
 let accumulated = 0;
-
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
 let expenses1 = prompt('Введите обязательную статью расходов?');
 let amount1 = +prompt('Во сколько это обойдётся?');
 let expenses2 = prompt('Введите обязательную статью расходов?');
@@ -23,33 +18,32 @@ let amount2 = +prompt('Во сколько это обойдётся?');
 const showTypeOf = function(data){
     console.log(data, typeof (data));
 }
-
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-const getExpensesMonth = function(){
-    expenses = amount1 + amount2;
+const getExpensesMonth = function(a, b){
+    expenses = a + b;
     return expenses;
 }
-getExpensesMonth();
+getExpensesMonth(amount1, amount2);
 
 console.log(' Общие расходы за месяц составляют: ' + expenses + ' долларов') ;
 console.log(addExpenses.toLowerCase().split(','));
 
-const getAccumulatedMonth = function(){
-    accumulated =  money - expenses;
+const getAccumulatedMonth = function(c, d){
+    accumulated =  c - d;
     return accumulated;
 }
-getAccumulatedMonth();
+getAccumulatedMonth(money, expenses);
 
 let accumulatedMonth = accumulated;
 
-const getTargetMonth = function(){
-    target = mission/accumulatedMonth;
+const getTargetMonth = function(e, f){
+    target = e/f;
     return target;
 }
-getTargetMonth();
+getTargetMonth(mission, accumulatedMonth);
 
 console.log('Цель будет достигнута за: '+ Math.ceil(target) +' месяца');
 
