471b5d263c7471fe96effd3c2b1f1d536b54518e
Andy Demichev <Andyuson24@gmail.com>
2021-01-22 21:36:10 +0200

    Create lesson19

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 
     // Timer
-   const countTimer = (deadline) =>{
+    const countTimer = (deadline) =>{
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', function(){
                 
                 return {timeRemaining, hours, minutes, seconds};
                 
-        }
+        };
 
     const updateClock = () =>{
             const timer = getTimeRemaining();
@@ -46,11 +46,78 @@ window.addEventListener('DOMContentLoaded', function(){
                 timerSeconds.textContent = '00';
             }  
             
-        }
+        };
 
         updateClock();
     };
 
     countTimer('23 january 2021');
 
+
+    //menu
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+                menu.classList.toggle('active-menu');
+
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+    //popup
+    const togglePpup = () =>{
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+
+            const screenWidth = document.documentElement.clientWidth;
+
+            const popupContent = document.querySelector('.popup-content');
+            const popleft = function(){
+                popup.style.display = 'block';
+                const start = Date.now(),
+                    timer = setInterval(function() {
+                    let timePassed = Date.now() - start;
+                    popupContent.style.left = timePassed / 2 + 'px';
+                    if (timePassed > 1500){
+                    clearInterval(timer);}
+            
+                }, 3);
+
+            };
+
+
+        popupBtn.forEach((elem) =>{
+            elem.addEventListener('click', ()=>{
+                
+                if(screenWidth > 768){
+                    popleft();  
+                }else{
+                    popupContent.style.left = 10;
+                    popup.style.display = 'block';
+                }
+            });
+            
+        
+
+
+
+
+        });
+        popUpClose.addEventListener('click', () =>{
+            popup.style.display = 'none';
+        });
+
+    
+    };
+    togglePpup();
 });
