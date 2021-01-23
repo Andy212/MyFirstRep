d2c800ecaceeb2c4e8718086cba03d5a00b5a90d
Andy212 <xfintx@tut.by>
2021-01-23 20:56:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -76,7 +76,11 @@ window.addEventListener('DOMContentLoaded', function(){
         const popup = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
             popUpClose = document.querySelector('.popup-close');
-
+       
+            let screenWidth = document.documentElement.clientWidth;
+          
+            window.addEventListener('resize', ()=>{
+                screenWidth = document.documentElement.clientWidth;});
 
             const screenWidth = document.documentElement.clientWidth;
 
@@ -97,13 +101,8 @@ window.addEventListener('DOMContentLoaded', function(){
 
         popupBtn.forEach((elem) =>{
             elem.addEventListener('click', ()=>{
-                
-                if(screenWidth > 768){
-                    popleft();  
-                }else{
-                    popupContent.style.left = 10;
-                    popup.style.display = 'block';
-                }
+                popup.style.display = 'block';
+                 if(screenWidth > 768) {popleft();}
             });
             
         
