d4e8a0241267d0e98c1d3d90de68f914156fe018
Andy Dzemichau <andyuson24@gmail.com>
2021-01-25 21:24:34 +0200

    make lesson24

--- script/script.js
+++ script/script.js
@@ -283,19 +283,71 @@ window.addEventListener('DOMContentLoaded', function(){
 		newCommand.addEventListener('mouseout', hidenPhotos);
 	};
 
+//calc
 
-    const calcFields = () => {
-        const inputFields = document.querySelector('.calc-block');
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+    const countSum = () =>{
+        let total = 0,
+        countValue = 1,
+        dayValue = 10;
+        const typeValue = calcType.options[calcType.selectedIndex].value,       
+            squareValue = +calcSquare.value;
+
+        if(calcCount.value > 1){
+            countValue += (calcCount.value - 1) / 10;
+        }
+    
+        if(calcDay.value && calcDay.value < 5){
+            dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10){
+            dayValue *= 1.5;
+        }
+
+        if(typeValue && squareValue){
+            total = price * typeValue * squareValue * countValue * dayValue;
+        }
+
+        totalValue.textContent = total;
+
+
+
+    }
         
-        console.log(inputFields)
-        inputFields.addEventListener('input', (event) =>{
-            event.target.value = event.target.value.replace(/\D/g, '');
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            if(target.matches('select') || target.matches('input')){
+
+                
+                countSum();
+            }
+            
         })
+
+        const calcFields = () => {
+            
+            calcBlock.addEventListener('input', (event) =>{
+                event.target.value = event.target.value.replace(/\D/g, '');
+            })
+        }
+        calcFields();
+
+
+
+
     }
 
     
     dotsAdd();
-    calcFields();
+    calc(100);
     slider();
     setCommandImg();
 });
